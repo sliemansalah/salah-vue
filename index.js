676d6609cf031ac600f5aa934a14d847d5393544
slieman <slieman2001@hotmail.com>
2020-12-11 21:26:48 +0300

    updated..

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import SCrump from "./src/SCrump.vue";
-import HeaderCrud from "./src/HeaderCrud.vue";
-import GroupBtns from "./src/GroupBtns.vue";
-import Test from "./src/Test.vue";
-
+import SHeader from "./src/SHeader.vue";
+import SHeaderBtns from "./src/GroupBtns.vue";
 
 export default {
   install(Vue, options) {
     Vue.component("SCrump", SCrump);
-    Vue.component("HeaderCrud", HeaderCrud);
-    Vue.component("GroupBtns", GroupBtns);
-    Vue.component("Test", Test);
+    Vue.component("SHeader", SHeader);
+    Vue.component("SHeaderBtns", SHeaderBtns);
   }
 };
